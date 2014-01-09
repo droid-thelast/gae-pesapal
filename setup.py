a7c983aced53d2b606776da2c927eb5ef1399b20
Mitchel Kelonye <kelonyemitchel@gmail.com>
2014-01-09 10:51:33 +0300

    use pesapal@0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gae-pesapal',
-    version='0.0.1',
+    version='0.0.2',
     description='Pesapal NDB Model',
     author='Mitchel Kelonye',
     author_email='kelonyemitchel@gmail.com',
